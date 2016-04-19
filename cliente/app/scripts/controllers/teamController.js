fe3bb193a6c9ccde7f0c7776ae2c408eb8abe9c9
gileadekelvin <gileade.silva@ccc.ufcg.edu.br>
2016-04-19 10:31:58 -0300

    adicionando informacoes em team

--- cliente/app/scripts/controllers/teamController.js
+++ cliente/app/scripts/controllers/teamController.js
@@ -10,8 +10,8 @@ angular.module("clienteApp").controller("teamCtrl", function($scope){
   var lucas = {nome: "Lucas", foto:"images/team/lucas.jpg", github: "https://github.com/jvmafra",
     mail: 	"joao.mafra@ccc.ufcg.edu.br", facebook: "#"};
 
-  var kelvin = {nome: "Gileade Kelvin", foto:"images/team/kelvin.jpg", github: "https://github.com/jvmafra",
-    mail:"joao.mafra@ccc.ufcg.edu.br", facebook: "#"};
+  var kelvin = {nome: "Gileade Kelvin", foto:"images/team/kelvin.jpg", github: "https://github.com/gileadekelvin",
+    mail:"gileade.silva@ccc.ufcg.edu.br", facebook: "https://www.facebook.com/gileadekelvin"};
 
   var rafael = {nome: "Rafael Albuquerque", foto:"images/team/rafa.jpg", github: "https://github.com/AlbuquerqueRafael/",
     mail: 	"rafael.sousa@ccc.ufcg.edu.br", facebook: "#/team"};
