4e44be1de1cf1c2445d4850c42a1aefac5067472
lucaspk <jobson.silva@ccc.ufcg.edu.br>
2016-04-19 18:17:56 -0300

    atualizaçao dos meus dados na parte de team

--- cliente/app/scripts/controllers/teamController.js
+++ cliente/app/scripts/controllers/teamController.js
@@ -7,8 +7,8 @@ angular.module("clienteApp").controller("teamCtrl", function($scope){
   var mafra = {nome: "João Mafra", foto:"images/team/mafra.jpg", github: "https://github.com/jvmafra",
                 mail: "joao.mafra@ccc.ufcg.edu.br", facebook: "#"};
 
-  var lucas = {nome: "Lucas", foto:"images/team/lucas.jpg", github: "https://github.com/jvmafra",
-    mail: 	"joao.mafra@ccc.ufcg.edu.br", facebook: "#"};
+  var lucas = {nome: "Lucas", foto:"images/team/lucas.jpg", github: "https://github.com/lucaspk",
+    mail: 	"jobson.silva@ccc.ufcg.edu.br", facebook: "https://www.facebook.com/jlucas.dias.1"};
 
   var kelvin = {nome: "Gileade Kelvin", foto:"images/team/kelvin.jpg", github: "https://github.com/gileadekelvin",
     mail:"gileade.silva@ccc.ufcg.edu.br", facebook: "https://www.facebook.com/gileadekelvin"};
