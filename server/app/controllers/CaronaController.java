4058b6a737ae4cd7903a71c106a037203b867a77
jvmafra <joao.mafra@ccc.ufcg.edu.br>
2016-04-20 18:42:17 -0300

    remocao antigo log

--- server/app/controllers/CaronaController.java
+++ server/app/controllers/CaronaController.java
@@ -32,8 +32,6 @@ public class CaronaController extends Controller {
 
         SistemaCaronas.getInstance().adicionarCarona(carona);
 
-        AdminController.novaCarona(carona);
-
 
         return ok("Carona cadastrada com sucesso!");
     }
@@ -49,8 +47,6 @@ public class CaronaController extends Controller {
         }
 
         SistemaSolicitacao.getInstance().adicionarSolicitacao(solicitacao);
-
-        AdminController.novaSolicitacao(solicitacao);
 
         return ok(Json.toJson(solicitacao)); // precisaria disso mesmo?
     }
@@ -68,7 +64,6 @@ public class CaronaController extends Controller {
             Usuario passageiro = solicitacao.getPassageiro();
             Carona carona = solicitacao.getCarona();
             carona.novoPassageiro(passageiro);
-            AdminController.novaSolicitacaoAceita(solicitacao);
             return ok(Json.toJson(solicitacao));
 
         } else {
@@ -82,8 +77,7 @@ public class CaronaController extends Controller {
         JsonNode json = request().body().asJson();
         Solicitacao solicitacao = Json.fromJson(json, Solicitacao.class);
 
-        if (solicitacao.setStatus(Status.REJEITADO)){   
-            AdminController.novaSolicitacaoRejeitada(solicitacao);                       // so pode recusar se a carona estiver pendente
+        if (solicitacao.setStatus(Status.REJEITADO)){                     // so pode recusar se a carona estiver pendente
             return ok(Json.toJson(solicitacao));
         } else {
             return badRequest("Você já tomou uma decisão sobre esse pedido");
