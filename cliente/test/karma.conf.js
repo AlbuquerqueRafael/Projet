5453617efa72ea31d7327369967d069312d934c5
AlbuquerqueRafael <rafael.sousa@ccc.ufcg.edu.br>
2016-04-21 17:14:43 -0300

    Up

--- cliente/test/karma.conf.js
+++ cliente/test/karma.conf.js
@@ -20,10 +20,7 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       // bower:js
-      'bower_components/jquery/jquery.js',
-      'bower_components/jquery-ui/ui/jquery-ui.js',
       'bower_components/angular/angular.js',
-      'bower_components/bootstrap/dist/js/bootstrap.js',
       'bower_components/angular-animate/angular-animate.js',
       'bower_components/angular-cookies/angular-cookies.js',
       'bower_components/angular-messages/angular-messages.js',
@@ -31,7 +28,6 @@ module.exports = function(config) {
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
-      'bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
       'bower_components/angularUtils-pagination/dirPagination.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
