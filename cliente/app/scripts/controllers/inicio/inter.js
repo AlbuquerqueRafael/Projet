171b73e520ac86ac9425a635f37ac666e9689a6c
gileadekelvin <gileade.silva@ccc.ufcg.edu.br>
2016-05-11 16:38:55 -0300

    arquivo inter.js

--- cliente/app/scripts/controllers/inicio/inter.js
+++ cliente/app/scripts/controllers/inicio/inter.js
@@ -21,6 +21,29 @@ $app.config([‘$translateProvider’, function ($translateProvider) {
     ‘main.senha.type’: ‘Type your password’,
     ‘main.cadastro’: ‘Sign up’,
 
+    ‘home.mudarFoto’: ‘Change your profile picture’,
+
+    ‘busca.paginaAnterior’: ‘Previous page’,
+    ‘busca.paginaPosterior’: ‘Next page’,
+    ‘busca.diaDaSemana’: ‘Day of the week’,
+    ‘busca.aula’: ‘Class’,
+    ‘busca.tipoDeCarona’: ‘Kind of ride’,
+    ‘busca.tipoDeCarona.ida’: ‘One-way trip’,
+    ‘busca.tipoDeCarona.volta’: ‘Back trip’,
+    ‘busca.endereco’: ‘Address’,
+    ‘busca.buscar’: ‘Search’,
+
+    ‘carona.diaDaSemana’: ‘Day of the week’,
+    ‘carona.aula’: ‘Class’,
+    ‘carona.vagas’: ‘Ride spaces’,
+    ‘carona.tipoDeCarona’: ‘Kind of ride’,
+    ‘carona.tipoDeCarona.ida’: ‘One-way trip’,
+    ‘carona.tipoDeCarona.volta’: ‘Back trip’,
+    ‘carona.endereco’: ‘Address’,
+    ‘carona.rotas’: ‘Routes’,
+    ‘carona.salvar’: ‘Save’,
+    ‘carona.cancelar’: ‘Cancel’,
+
 
     ‘label.title’: ‘Vote in Movie?’,   
   });
@@ -40,12 +63,34 @@ $app.config([‘$translateProvider’, function ($translateProvider) {
 	‘cadastro.telefone.ddd’: ‘Telefone sem DDD’,
 	‘cadastro.telefone.check’: ‘Digite os números do seu telefone’,
 
-
     ‘main.email’: ‘Email’,
     ‘main.email.type’: ‘Digite seu email’,
     ‘main.senha’: ‘Senha’,
     ‘main.senha.type’: ‘Digite a senha’,
     ‘main.cadastro’: ‘Cadastrar’,
+
+    ‘home.mudarFoto’: ‘Mudar foto de perfil’,
+
+    ‘busca.paginaAnterior’: ‘Página anterior’,
+    ‘busca.paginaPosterior’: ‘Próxima página’,
+    ‘busca.diaDaSemana’: ‘Dia da semana’,
+    ‘busca.aula’: ‘Aula’,
+    ‘busca.tipoDeCarona’: ‘Tipo de Carona’,
+    ‘busca.tipoDeCarona.ida’: ‘IDA’,
+    ‘busca.tipoDeCarona.volta’: ‘VOLTA’,
+    ‘busca.endereco’: ‘Endereço’,
+    ‘busca.buscar’: ‘Buscar’,
+
+    ‘carona.diaDaSemana’: ‘Dia da Semana’,
+    ‘carona.aula’: ‘Aula’,
+    ‘carona.vagas’: ‘Vagas’,
+    ‘carona.tipoDeCarona’: ‘Tipo de carona’,
+    ‘carona.tipoDeCarona.ida’: ‘IDA’,
+    ‘carona.tipoDeCarona.volta’: ‘VOLTA’,
+    ‘carona.endereco’: ‘Endereço’,
+    ‘carona.rotas’: ‘Rotas’,
+    ‘carona.salvar’: ‘Salvar’,
+    ‘carona.cancelar’: ‘Cancelar’,
 
 
     ‘label.title’: ‘Vote no Filme’   
