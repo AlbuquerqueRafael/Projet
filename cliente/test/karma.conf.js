02752f543f37be40a422b7fe84e6afb23f9b2c8f
gileadekelvin <gileade.silva@ccc.ufcg.edu.br>
2016-05-11 15:13:22 -0300

    adicao do angular-translate

--- cliente/test/karma.conf.js
+++ cliente/test/karma.conf.js
@@ -36,6 +36,7 @@ module.exports = function(config) {
       'bower_components/angular-upload/angular-upload.js',
       'bower_components/ng-file-upload/ng-file-upload.js',
       'bower_components/ng-file-upload-shim/ng-file-upload-shim.js',
+      'bower_components/angular-translate/angular-translate.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
