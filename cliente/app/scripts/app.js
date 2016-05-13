98a756fac9413df683ceff8f3594fcf65b280ff3
gileadekelvin <gileade.silva@ccc.ufcg.edu.br>
2016-05-13 10:51:39 -0300

    inicio de internacionalizacao

--- cliente/app/scripts/app.js
+++ cliente/app/scripts/app.js
@@ -87,18 +87,48 @@ angular
 
 angular.module("clienteApp").config(function($translateProvider) {
   $translateProvider.translations('en', {
-      'label.question.movie': "What is your favorite movie?",
+      'label.question.movie': "What is your favorite movie?",      
+      'cadastro.title': "Sign up!",
+      'cadastro.nome': "Name",
+      'cadastro.email': "Email",
+      'cadastro.email.check': "You must enter a valid email address. Example caroname@caroname.com",
+      'cadastro.senha': "Password",
+      'cadastro.senha.check': "At least 8 characters",
+      'cadastro.matricula': "Number of enrollment",
+      'cadastro.matricula.check': "Type the 9 numbers of enrollment in UFCG",
+      'cadastro.rua': "Address",
+      'cadastro.telefone': "Phone number",
+      'cadastro.telefone.ddd': "Phone number without DDD",
+      'cadastro.telefone.check': "Type your phone number", 
       'label.title': "Vote in Movie?"
   });
-  $translateProvider.translations('pt', {
+  $translateProvider.translations('pt', {      
+      'cadastro.title': "Cadastro!",
       'label.question.movie': "Qual filme você gosta mais?",
+      'cadastro.nome': "Nome",
+      'cadastro.email': "Email",
+      'cadastro.email.check': "Preencha com um email válido. Exemplo caroname@caroname.com",
+      'cadastro.senha': "Senha",
+      'cadastro.senha.check': "Pelo menos 8 caracteres",
+      'cadastro.matricula': "Nº Matricula",
+      'cadastro.matricula.check': "Digite os 9 números da sua matrícula da UFCG",
+      'cadastro.rua': "Rua",
+      'cadastro.telefone': "Telefone",
+      'cadastro.telefone.ddd': "Telefone sem DDD",
+      'cadastro.telefone.check': "Digite os números do seu telefone",
       'label.title': "Vote no Filme"
   });
 
-  $translateProvider.preferredLanguage('en'); //temos que dizer qual é a default.
+  $translateProvider.preferredLanguage('pt'); //temos que dizer qual é a default.
 });
 
-
+angular.module('clienteApp').controller('Ctrl', ['$translate', '$scope', function ($translate, $scope) {
+ 
+  $scope.changeLanguage = function (langKey) {
+    $translate.use(langKey);
+  };
+ 
+}]);
 
 
 angular.module("clienteApp").run(function($rootScope, $location, menuService, mainService){
